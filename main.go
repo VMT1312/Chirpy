8424a3d3300f16059981c8529e964e4e0110c6f4
VMT1312 <thiemmavinh123@gmail.com>
2025-07-12 16:20:46 +0800

    Remove unused functions and streamline API endpoint handling in main.go

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"sync/atomic"
 )
 
 func main() {
@@ -18,48 +16,12 @@ func main() {
 
 	mux.HandleFunc("POST /admin/reset", apiCfg.resetServerHits)
 
+	mux.HandleFunc("POST /api/validate_chirp", requestHandler)
+
 	server := &http.Server{
 		Handler: mux,
 		Addr:    ":8080",
 	}
 
 	server.ListenAndServe()
-}
-
-func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
-}
-
-type apiConfig struct {
-	fileserverHits atomic.Int32
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request) {
-			cfg.fileserverHits.Add(1)
-			next.ServeHTTP(w, r)
-		},
-	)
-}
-
-func (cfg *apiConfig) getFileserverHits(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	w.Write([]byte(fmt.Sprintf(
-		`<html>
-  			<body>
-				<h1>Welcome, Chirpy Admin</h1>
-				<p>Chirpy has been visited %d times!</p>
-			</body>
-		</html>`,
-		cfg.fileserverHits.Load(),
-	)),
-	)
-}
-
-func (cfg *apiConfig) resetServerHits(w http.ResponseWriter, r *http.Request) {
-	cfg.fileserverHits.Store(0)
-	w.Write([]byte("Fileserver hits reset"))
 }
