3297b260d3d6d6777742edd011f876f35b20a43d
VMT1312 <thiemmavinh123@gmail.com>
2025-07-12 16:20:39 +0800

    Move health check endpoint and  apiConfig for metrics tracking for readability and maintainability.

---
+++ type_apiConfig.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			cfg.fileserverHits.Add(1)
+			next.ServeHTTP(w, r)
+		},
+	)
+}
+
+func (cfg *apiConfig) getFileserverHits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte(fmt.Sprintf(
+		`<html>
+  			<body>
+				<h1>Welcome, Chirpy Admin</h1>
+				<p>Chirpy has been visited %d times!</p>
+			</body>
+		</html>`,
+		cfg.fileserverHits.Load(),
+	)),
+	)
+}
+
+func (cfg *apiConfig) resetServerHits(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte("Fileserver hits reset"))
+}
