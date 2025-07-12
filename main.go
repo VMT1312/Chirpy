563264314270324553769c3ef7d77f43c411098d
VMT1312 <thiemmavinh123@gmail.com>
2025-07-12 15:48:34 +0800

    Implement API endpoints for health check, metrics, and reset; add middleware for file server hit tracking

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
 
 func main() {
 	mux := http.NewServeMux()
+	apiCfg := &apiConfig{}
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("./app"))))
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir("./app")))))
 
-	mux.HandleFunc("/healthz", healthCheckHandler)
+	mux.HandleFunc("GET /api/healthz", healthCheckHandler)
+
+	mux.HandleFunc("GET /api/metrics", apiCfg.getFileserverHits)
+
+	mux.HandleFunc("POST /api/reset", apiCfg.resetServerHits)
 
 	server := &http.Server{
 		Handler: mux,
@@ -21,4 +30,26 @@ func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			cfg.fileserverHits.Add(1)
+			next.ServeHTTP(w, r)
+		},
+	)
+}
+
+func (cfg *apiConfig) getFileserverHits(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) resetServerHits(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte("Fileserver hits reset"))
 }
